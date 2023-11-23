e602c46099d3e7e56d6f9f169798353158462ff0
雷育林 <lyl694550795@msn.cn>
2023-11-24 01:01:00 +0800

    Remove useless func

--- geekbang/src/ch1/slice_remove_test.go
+++ geekbang/src/ch1/slice_remove_test.go
@@ -23,16 +23,7 @@ func Remove[T any](s []T, index int) (res []T, err error) {
 	copy(s1[index:len(s1)], s[index+1:len(s)])
 	return s1, nil
 }
-func Remove_1[T any](s []T, index int) (res []T, err error) {
-	if index > len(s)-1 || index < 0 {
-		return s, errors.New("Index > Slice len or index < 0")
-	}
-	s1 := s[:index]
-	for _, value := range s[index+1:] {
-		s1 = append(s1, value)
-	}
-	return s1, nil
-}
+
 func StringSlice_remove() {
 	s1 := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"}
 	s2, _ := Remove(s1, 3)
@@ -45,31 +36,13 @@ func StringSlice_remove() {
 	fmt.Println(s5, cap(s5))
 	s6, _ := Remove(s5, 4)
 	fmt.Println(s6, cap(s6))
-	s7, _ := Remove_1(s6, len(s6)-1)
+	s7, _ := Remove(s6, len(s6)-1)
 	fmt.Println(s7, cap(s7))
 	s8, _ := Remove(s7, 3)
 	fmt.Println(s8, cap(s8))
 }
-func StringSlice_remove_1() {
-	s1 := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"}
-	s2, _ := Remove_1(s1, 3)
-	fmt.Println(s2, cap(s2))
-	s3, _ := Remove_1(s2, 0)
-	fmt.Println(s3, cap(s3))
-	s4, _ := Remove_1(s3, 3)
-	fmt.Println(s4, cap(s4))
-	s5, _ := Remove_1(s4, -1)
-	fmt.Println(s5, cap(s5))
-	s6, _ := Remove_1(s5, 4)
-	fmt.Println(s6, cap(s6))
-	s7, _ := Remove_1(s6, len(s6)-1)
-	fmt.Println(s7, cap(s7))
-	s8, _ := Remove_1(s7, 3)
-	fmt.Println(s8, cap(s8))
-}
 func BenchmarkSlice(b *testing.B) {
 	b.StartTimer()
-	//StringSlice_remove_1() //BenchmarkSlice-24       1000000000               0.001040 ns/op
-	StringSlice_remove() //BenchmarkSlice-24       1000000000               0.001021 ns/op
+	StringSlice_remove() //BenchmarkSlice-24       1000000000               0.0005196 ns/op
 	b.StopTimer()
 }
